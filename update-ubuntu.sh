97ad293c8dda9adcca148805b5577cb2e9d23aed
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-11-21 16:12:15 +0100

    fix aptitude install

--- update-ubuntu.sh
+++ update-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # wget -qO- https://raw.github.com/obazoud/linux-bootstrap/master/update-ubuntu.sh | sh
 
-sudo aptitude --assume-yes install aptitude
+sudo apt-get --assume-yes install aptitude
 sudo aptitude --assume-yes --quiet update
 sudo aptitude --assume-yes --quiet safe-upgrade
 sudo aptitude --assume-yes --quiet install python-software-properties openssh-server curl
